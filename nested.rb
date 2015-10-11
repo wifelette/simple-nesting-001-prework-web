6333d08af2f7d8d58bbbe36a38967d090f94a07e
wifelette <leahsilber@gmail.com>
2015-10-11 16:30:50 -0700

    Passing

--- nested.rb
+++ nested.rb
@@ -14,9 +14,8 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
-
+    }
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +36,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +83,12 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP, C"]
+  }
+
+  programmer_hash 
 end
 
 def changing_alan
@@ -99,10 +107,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for the value of the alans_new_info variable. 
+     
      alans_new_info = "GUI"
-     
-     
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -121,8 +129,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
-     
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  programmer_hash
 end
 
 
